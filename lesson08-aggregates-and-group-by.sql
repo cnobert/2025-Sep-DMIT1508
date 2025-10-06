c862143110409f98608aec70134308c2854b7aad
cnobert <cnobert@nait.ca>
2025-10-06 10:43:45 -0600

    Update lesson08-aggregates-and-group-by.sql

--- lesson08-aggregates-and-group-by.sql
+++ lesson08-aggregates-and-group-by.sql
@@ -11,9 +11,43 @@ select MIN(DateOfBirth), MAX(DateOfBirth)
 from Student
 
 --Average floor number of lockers
-select avg(LockerId)
+select avg(Floor)
 from Locker
 
+--every column in the select must either be in the "group by" or 
+--in an aggregate function
 select building, count(*)
 from Locker
 group by building
+
+--return how many lockers are on each floor
+select Locker.Floor, count(*) 'Lockers per floor'
+from Locker
+group by Floor
+
+--how many phone numbers for each student ID
+select StudentID, count(*) 'Phones per student'
+from Phone 
+group by StudentId
+
+--let's make the above query more user friendly
+--how many phones for each Student name
+select s.FirstName, s.LastName, count(p.PhoneNumber) 'Number of Phones'
+from Phone p join Student s on p.StudentID = s.StudentID
+group by s.FirstName, s.LastName  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

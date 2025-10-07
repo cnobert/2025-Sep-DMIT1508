45bc7c9f12ced46a7e2c583a93adc247e0d44ee0
cnobert <cnobert@nait.ca>
2025-10-07 14:05:11 -0600

    Create lesson10-FullDB-Union-Subqueries.sql

---
+++ lesson10-FullDB-Union-Subqueries.sql
@@ -0,0 +1,45 @@
+select * from course
+select * from enrollment
+
+--IN CLASS EXERCISES
+
+--return course code and student name of students who are enrolled in a course
+select s.FirstName, s.LastName, c.CourseCode 
+from Student s join enrollment e on s.StudentID = e.StudentID 
+join Course c on e.CourseID = c.CourseID 
+
+--return course code and name of students who are enrolled in a course
+--that is in the Game Programming Department
+select s.FirstName, s.LastName, c.CourseCode 
+from Student s join enrollment e on s.StudentID = e.StudentID 
+join Course c on e.CourseID = c.CourseID 
+where c.Department = 'Game Programming'
+
+--return the student name, assessment title, and points earned
+--for all assessments
+--don't show assessments with no points earned
+--order by last name and points earned, with highest points earned at the top
+select s.FirstName , s.LastName, a.Title , a.PointsEarned 
+from Student s join enrollment e on s.StudentID = e.StudentID
+join Assessment a on e.EnrollmentID = a.EnrollmentID
+where a.PointsEarned is not null
+order by s.LastName , a.PointsEarned desc
+
+--show student names and course codes of students 
+--who are active in DMIT1508
+select s.FirstName, s.LastName, c.CourseCode 
+from Student s join enrollment e on s.StudentID = e.StudentID 
+join Course c on e.CourseID = c.CourseID
+where Status = 'Active' and c.CourseCode = 'DMIT1508'
+
+--return the average final grade
+select avg(FinalGradePercent)
+from Enrollment
+
+--return the course code and average final grade for each course
+select c.CourseCode, avg(e.FinalGradePercent)
+from Enrollment e join Course c on e.CourseID = c.CourseID 
+where e.FinalGradePercent is not null
+group by c.CourseCode 
+
+

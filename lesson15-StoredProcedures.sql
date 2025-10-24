c5a60d5f5f4b0bce62ceb06475ec05de04476a46
cnobert <cnobert@nait.ca>
2025-10-24 10:57:25 -0600

    Update lesson15-StoredProcedures.sql

--- lesson15-StoredProcedures.sql
+++ lesson15-StoredProcedures.sql
@@ -9,7 +9,7 @@ if @studentID is null
 		raiserror('StudentID cannot be null.', 16, 1);
 	end
 --will return false if the studentID isn't in the database
-if not exists(select 1 from Student where StudentID = @studentID)
+else if not exists(select 1 from Student where StudentID = @studentID)
 	begin
 		raiserror('hey silly, StudentID not found.', 16, 1);
 	end
@@ -22,3 +22,50 @@ else
 
 
 exec GetStudentByID 23423423
+exec GetStudentByID @studentID = null
+
+drop procedure GetLockerForStudent
+go 
+create procedure GetLockerForStudent
+	@studentID int 
+as
+if @studentID is NULL 
+	begin
+		RAISERROR('StudentID cannot be null.',16,1);
+	end
+else if not exists(select 1 from Student where StudentID = @studentID)
+	begin
+		raiserror('hey silly, StudentID not found.', 16, 1);
+	end
+else if not exists(select 1 from Locker where StudentID = @studentID)
+	begin
+		raiserror('No locker is assigned to this student.', 16, 1);
+	end
+else
+	begin
+		SELECT LockerID, LockerNumber, Building, Floor
+		FROM dbo.Locker
+		WHERE StudentID = @StudentID;
+	end
+	
+exec GetLockerForStudent @studentID = null
+exec GetLockerForStudent 234234
+exec GetLockerForStudent 7
+exec GetLockerForStudent 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
